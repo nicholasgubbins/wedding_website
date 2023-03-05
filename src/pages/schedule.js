8fbfd16407d5cae50c54fd5b27d2f0fca2d2b832
Nick Gubbins <nick@atellio.com>
2023-03-05 21:22:40 +0000

    change dates

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -9,7 +9,7 @@ export default (props) => (
         <i class="fa-solid fa-pizza-slice fa-2xl inline-icons" />
         <h2>Pizza Party</h2>
         <h4>Friday 15 September, 2023</h4>
-        <h4>6:30pm-10:00pm</h4>
+        <h4>7:30pm</h4>
         <h3>Ortigia</h3>
         <h4>Via Saverio Landolina, 6, 96100 Siracusa SR, Italy</h4>
         <h5>Casual</h5>
@@ -23,7 +23,7 @@ export default (props) => (
         <i class="fa-solid fa-champagne-glasses fa-2xl inline-icons" />
         <h2>The Big Day</h2>
         <h4>Saturday 16 September, 2023</h4>
-        <h4>3pm-late</h4>
+        <h4>4pm-late</h4>
         <h3>Borgo del Carato</h3>
         <h4>S.S. 124 Km 97, 96010 Palazzolo Acreide SR, Italy</h4>
         <h5>Mediterranean Chic</h5>
